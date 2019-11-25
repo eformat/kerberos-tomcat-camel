8983713724b663ffb7a879edb3afa7160773fd04
Mike Hepburn <eformat@gmail.com>
2019-11-25 12:40:05 +1000

    🎉 tomcat 🎉

--- src/main/java/com/example/JDBCKerberosProcessor.java
+++ src/main/java/com/example/JDBCKerberosProcessor.java
@@ -3,21 +3,22 @@ package com.example;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import javax.jms.QueueConnectionFactory;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class JDBCKerberosProcessor implements Processor {
 
-    /* Kerberos */
-    private String connectionUrl = "jdbc:sqlserver://localhost;databaseName=model;integratedSecurity=true;authenticationScheme=JavaKerberos";
-
-    /* Local connection username, password */
-    //private String connectionUrlLocal = "jdbc:sqlserver://localhost:1433;databaseName=model;user=sa;password=password;";
+    private Context ctx = null;
+    private DataSource ds = null;
 
     public void process(Exchange exchange) throws Exception {
         System.out.println("In JDBCKerberosProcessor");
 
         // Declare the JDBC objects.
-        Connection con = null;
+
         Statement stmt = null;
         ResultSet rs = null;
 
@@ -26,11 +27,9 @@ public class JDBCKerberosProcessor implements Processor {
         System.setProperty("java.security.auth.login.config", "/opt/webserver/conf/SQLJDBCDriver.conf");
 
         try {
-            // Establish the connection.
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-            con = DriverManager.getConnection(connectionUrl);
-            DatabaseMetaData dbmd = con.getMetaData();
+            ctx = (Context) new InitialContext().lookup("java:comp/env");
+            ds = (DataSource) ctx.lookup("jdbc/MySQLServer");
+            DatabaseMetaData dbmd = ds.getConnection().getMetaData();
 
             System.out.println("dbmd:driver version = " + dbmd.getDriverVersion());
             System.out.println("dbmd:driver name = " + dbmd.getDriverName());
